c92d0daf65944bf2c498e4679068c628e8371b72
febat <febathedeveloper@gmail.com>
2024-03-06 08:14:08 -0500

    Add project files.

---
+++ TriangleSolverUnitTest/TriangleSolverTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TriangleSolver;
+
+namespace TriangleSolverUnitTest
+{
+    [TestFixture]
+    public class TriangleSolverTests
+    {
+        [Test]
+
+        public void ValidTriangle_Input_7and10and5_OutputScaleneTriange() {
+            //Arrange
+            int side1 = 7;
+            int side2 = 10;
+            int side3 = 5;
+
+            String expected = "A scalene triangle is formed";
+
+            //Act
+            String actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expected));
+
+        }
+    }
+}
