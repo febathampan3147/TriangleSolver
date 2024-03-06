a303dacf88b154fac7b3e9b2f6d26f8aa6967ed5
febat <fthampan3147@conestogac.on.ca>
2024-03-06 09:11:07 -0500

    equilateral triangle test case added

--- TriangleSolverUnitTest/TriangleSolverTests.cs
+++ TriangleSolverUnitTest/TriangleSolverTests.cs
@@ -11,21 +11,42 @@ namespace TriangleSolverUnitTest
     [TestFixture]
     public class TriangleSolverTests
     {
-        [Test]
+        public static String equilateralTriangleMessage = "An equilateral triangle is formed";
+        public static String scaleneTriangleMessage = "A scalene triangle is formed";
+        public static String isoscelesTriangleMessage = "An isosceles triangle is formed";
+        public static String invalidTriangleMessage = "At least one side entered had a zero - invalid triangle";
+        public static String triangleNotFormedMessage = "A triangle cannot be formed";
 
-        public void ValidTriangle_Input_7and10and5_OutputScaleneTriange() {
+        [Test]
+        //Equilateral Triange Test
+        public void AnalyzeTriangle_Input6_OutputScaleneTriange()
+        {
+            //Arrange
+            int side1 = 6; // All 3 sides equal in an equilateral triangle
+
+            //Act
+            String actual = Triangle.AnalyzeTriangle(side1, side1, side1);
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(equilateralTriangleMessage));
+
+        }
+
+        //Scalene Triangle Test
+        [Test]
+        public void AnalyzeTriangle_Input_7and10and5_OutputScaleneTriange() {
             //Arrange
             int side1 = 7;
             int side2 = 10;
             int side3 = 5;
 
-            String expected = "A scalene triangle is formed";
+            //expected message : "A scalene triangle is formed";
 
             //Act
             String actual = Triangle.AnalyzeTriangle(side1, side2, side3);
 
             //Assert
-            Assert.That(actual, Is.EqualTo(expected));
+            Assert.That(actual, Is.EqualTo(scaleneTriangleMessage));
 
         }
     }
